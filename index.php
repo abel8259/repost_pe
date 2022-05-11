989887c50b6e013c4d2eb15d72327dc6a639f95d
Abel VasconcelosB <78666074+abel8260@users.noreply.github.com>
2022-05-11 07:03:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,1 @@
-<?php
-	echo "<a href='home/index.php'><img width='33%' height='auto' src='img_geral/begin.png' style='left:33%; top:9%; position:absolute; '/></a>";
-?>
+<a href='home/index.php'><img width='33%' height='auto' src='img_geral/begin.png' style='left:33%; top:9%; position:absolute; '/></a>
